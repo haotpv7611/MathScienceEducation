515acc71f5d4a3af8d7fd7780b5335c0d62ee682
haotpv7611 <63042398+haotpv7611@users.noreply.github.com>
2021-03-21 18:33:54 +0700

    feat: delete file in firebase, catch exceed limited exception, refractor api firebase

--- src/main/java/com/example/demo/controllers/FirebaseController.java
+++ src/main/java/com/example/demo/controllers/FirebaseController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controllers;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,19 +19,33 @@ import com.example.demo.services.impls.FirebaseService;
 public class FirebaseController {
 
 	@Autowired
-    private FirebaseService firebaseConfigService;
-    
-    @PostMapping("/api/v1/test")
-    public ResponseEntity<String> create(@RequestParam(name = "file") MultipartFile file) {
-    	String fileName = "";
-        try {
-            fileName = firebaseConfigService.saveFile(file);
-            // do whatever you want with that
-        } catch (Exception e) {
-        	System.out.println(e.getMessage());
-        	return ResponseEntity.status(HttpStatus.OK).body("Error here");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(fileName);
-    }
-  
+	private FirebaseService firebaseService;
+
+	@PostMapping("/file")
+	public ResponseEntity<String> create(@RequestParam MultipartFile multipartFile) throws IOException {
+
+		String response = firebaseService.saveFile(multipartFile);
+
+		if (response.contains("Not supported")) {
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+		if (response.contains("Invalid")) {
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+	@DeleteMapping("/file")
+	public ResponseEntity<String> delete(@RequestParam String fileUrl) {
+		String response = firebaseService.deleteFile(fileUrl);
+		
+		if (response.contains("invalid")) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
 }
