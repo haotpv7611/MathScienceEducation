bf5b8a82c1fa4d7f82eb55b7147ed76da26ffda0
haotpv7611 <63042398+haotpv7611@users.noreply.github.com>
2021-03-28 20:38:53 +0700

    fix: conflict name

--- src/main/java/com/example/demo/repositories/iGradeRepository.java
+++ src/main/java/com/example/demo/repositories/iGradeRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +7,5 @@ import com.example.demo.models.Grade;
 
 @Repository
 public interface iGradeRepository extends JpaRepository<Grade, Long> {
-
-	List<Grade> findByIsDisable(boolean isDisable);
+	
 }
