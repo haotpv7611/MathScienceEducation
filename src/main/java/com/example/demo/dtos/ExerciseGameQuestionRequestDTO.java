d9edacb67562ac95b33bc4d0c893e15a450ead7f
haotpv7611 <63042398+haotpv7611@users.noreply.github.com>
2021-04-08 23:34:28 +0700

    fix: add exerciseGameQuestionRequestDTO

---
+++ src/main/java/com/example/demo/dtos/ExerciseGameQuestionRequestDTO.java
@@ -0,0 +1,67 @@
+package com.example.demo.dtos;
+
+import java.util.List;
+
+public class ExerciseGameQuestionRequestDTO {
+	private List<Long> questionIds;
+	private long exerciseId;
+	private long gameId;
+	private boolean isExercise;
+
+	/**
+	 * @return the questionIds
+	 */
+	public List<Long> getQuestionIds() {
+		return questionIds;
+	}
+
+	/**
+	 * @param questionIds the questionIds to set
+	 */
+	public void setQuestionIds(List<Long> questionIds) {
+		this.questionIds = questionIds;
+	}
+
+	/**
+	 * @return the exerciseId
+	 */
+	public long getExerciseId() {
+		return exerciseId;
+	}
+
+	/**
+	 * @param exerciseId the exerciseId to set
+	 */
+	public void setExerciseId(long exerciseId) {
+		this.exerciseId = exerciseId;
+	}
+
+	/**
+	 * @return the gameId
+	 */
+	public long getGameId() {
+		return gameId;
+	}
+
+	/**
+	 * @param gameId the gameId to set
+	 */
+	public void setGameId(long gameId) {
+		this.gameId = gameId;
+	}
+
+	/**
+	 * @return the isExercise
+	 */
+	public boolean isExercise() {
+		return isExercise;
+	}
+
+	/**
+	 * @param isExercise the isExercise to set
+	 */
+	public void setExercise(boolean isExercise) {
+		this.isExercise = isExercise;
+	}
+
+}
