70878da99441ba95a3d2b4c027c40db8683b2ace
haotpv7611 <63042398+haotpv7611@users.noreply.github.com>
2021-04-10 12:16:40 +0700

    fix: open firebase controller

--- src/main/java/com/example/demo/controllers/FirebaseController.java
+++ src/main/java/com/example/demo/controllers/FirebaseController.java
@@ -21,31 +21,31 @@ public class FirebaseController {
 	@Autowired
 	private FirebaseService firebaseService;
 
-//	@PostMapping("/file")
-//	public ResponseEntity<String> create(@RequestParam MultipartFile multipartFile) throws IOException {
-//
-//		String response = firebaseService.saveFile(multipartFile);
-//
-//		if (response.contains("Not supported")) {
-//
-//			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-//		}
-//		if (response.contains("Invalid")) {
-//
-//			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-//		}
-//
-//		return ResponseEntity.status(HttpStatus.OK).body(response);
-//	}
+	@PostMapping("/file")
+	public ResponseEntity<String> create(@RequestParam MultipartFile multipartFile) throws IOException {
 
-//	@DeleteMapping("/file")
-//	public ResponseEntity<String> delete(@RequestParam String fileUrl) {
-//		String response = firebaseService.deleteFile(fileUrl);
-//		
+		String response = firebaseService.saveFile(multipartFile);
+
+		if (response.contains("Not supported")) {
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+		if (response.contains("Invalid")) {
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+	@DeleteMapping("/file")
+	public ResponseEntity<String> delete(@RequestParam String fileUrl) {
+		firebaseService.deleteFile(fileUrl);
+		
 //		if (response.contains("invalid")) {
 //			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
 //		}
-//		
-//		return ResponseEntity.status(HttpStatus.OK).body(response);
-//	}
+		
+		return ResponseEntity.status(HttpStatus.OK).body("SUCCESS!");
+	}
 }
