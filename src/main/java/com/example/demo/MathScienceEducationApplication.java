35be8024691d96ba4c8ac82a016be8772683abb8
haotpv7611 <63042398+haotpv7611@users.noreply.github.com>
2021-04-21 16:05:05 +0700

    fix: global cors

--- src/main/java/com/example/demo/MathScienceEducationApplication.java
+++ src/main/java/com/example/demo/MathScienceEducationApplication.java
@@ -3,12 +3,26 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MathScienceEducationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MathScienceEducationApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("http://localhost:3000").exposedHeaders("Content-Disposition")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
 	}
 
 }
