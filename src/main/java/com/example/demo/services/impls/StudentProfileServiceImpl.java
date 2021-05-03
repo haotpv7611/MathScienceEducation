286a5a4092b1fca69506b5aded3663e90db3d5a4
VnVentX <anhtt110698@gmail.com>
2021-05-03 21:30:06 +0700

    Revert "Update align excel file"
    This reverts commit 6241b5920b67d5c48201952446173c5874efa39b.

--- src/main/java/com/example/demo/services/impls/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/services/impls/StudentProfileServiceImpl.java
@@ -1166,11 +1166,11 @@ public class StudentProfileServiceImpl implements IStudentProfileService {
 								fullNameValueCell.setCellValue(studentProfileList.get(i).getAccount().getFullName());
 
 								Cell usernameValueCell = createOneNormalCell(workbook, sheet.getRow(i + 7), 3,
-										CellType.STRING, HorizontalAlignment.LEFT);
+										CellType.STRING, HorizontalAlignment.CENTER);
 								usernameValueCell.setCellValue(studentProfileList.get(i).getAccount().getUsername());
 
 								Cell passwordValueCell = createOneNormalCell(workbook, sheet.getRow(i + 7), 4,
-										CellType.STRING, HorizontalAlignment.LEFT);
+										CellType.STRING, HorizontalAlignment.CENTER);
 								passwordValueCell.setCellValue(DEFAULT_PASSWORD);
 							}
 						}
